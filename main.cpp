6be622a6b03371ff079147e8e0223be4ac2539a0
Ranaihab <rana.ihab7@gmail.com>
2022-05-27 21:50:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <stdio.h>
 
 using namespace std;
-FILE *g_ic_file_cout_stream; FILE *g_ic_file_cin_stream;
 
 enum Action{
 	parametricLine, dDALine, bresenhamLine, 
@@ -467,8 +466,8 @@ LRESULT WINAPI MyWndProc(HWND hwnd, UINT mcode, WPARAM wp, LPARAM lp)
 bool InitConsole()
 {
     if (!AllocConsole()) { return false; }
-    if (freopen_s(&g_ic_file_cout_stream, "CONOUT$", "w", stdout) != 0) { return false; } // For std::cout
-    if (freopen_s(&g_ic_file_cin_stream, "CONIN$", "w+", stdin) != 0) { return false; } // For std::cin
+    if (freopen("CONOUT$", "w", stdout) != 0) { return false; } // For std::cout
+    if (freopen("CONIN$", "w+", stdin) != 0) { return false; } // For std::cin
     return true;
 }
 int APIENTRY WinMain(HINSTANCE hinst, HINSTANCE pinst, LPSTR cmd, int nsh)
